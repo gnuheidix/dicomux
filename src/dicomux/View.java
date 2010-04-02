5d106b684e16f5ba2fddacd6ef093a5e4a4f8c3d
linustorvalds <heidixp@gmx.de>
2010-04-02 05:37:49 -0700

    SpringLayout added // JExpander preparations done

--- src/dicomux/View.java
+++ src/dicomux/View.java
@@ -2,6 +2,7 @@ package dicomux;
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 public class View extends JFrame {
@@ -9,9 +10,18 @@ public class View extends JFrame {
 	private static final long serialVersionUID = -3586989981842552511L;
 	private JTabbedPane m_tabbedPane;
 	private JMenuBar m_menuBar;
+	private JPanel m_patientPanel;
 
 	public View() {
 		setTitle("dicomux");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// extract own contentPane and set its layout manager
+		Container contentPane = getContentPane();
+		SpringLayout layout = new SpringLayout();
+		contentPane.setLayout(layout);
+		
+		// create a main menu and fill in some entries
 		m_menuBar = new JMenuBar();
 		JMenu menu = new JMenu("File");
 		menu.add(new JMenuItem("Open"));
@@ -24,26 +34,92 @@ public class View extends JFrame {
 		m_menuBar.add(menu);
 		setJMenuBar(m_menuBar);
 		
+		// create a tabbed pane and add it to the content pane
 		m_tabbedPane = new JTabbedPane();
+		m_tabbedPane.setPreferredSize(new Dimension(300, 200));
 		JComponent panel1 = makeTextPanel("Panel #1");
+//		panel1.setPreferredSize(new Dimension(200, 200));
 		m_tabbedPane.addTab("Tab 1", panel1);
 		m_tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-
-		JComponent panel2 = makeTextPanel("Panel #2");
-		panel2.setPreferredSize(new Dimension(800, 600));
-		m_tabbedPane.addTab("Tab 2", panel2);
-		m_tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-
 		m_tabbedPane.setTabPlacement(JTabbedPane.LEFT);
-		add(m_tabbedPane);
+		contentPane.add(m_tabbedPane);
 		
+		// create a panel for the patient data and add it to the content pane
+		m_patientPanel = new JExpander();
+//		m_patientPanel.setPreferredSize(new Dimension(50, 50));
+		contentPane.add(m_patientPanel);
+		
+		// adjust layout constraints of the components
+		SpringLayout.Constraints contentPaneCons = layout.getConstraints(contentPane);
+		SpringLayout.Constraints tabbedPaneCons = layout.getConstraints(m_tabbedPane);
+		SpringLayout.Constraints patientPaneCons = layout.getConstraints(m_patientPanel);
+		contentPaneCons.setHeight(Spring.sum(patientPaneCons.getConstraint(SpringLayout.HEIGHT),
+												tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)));
+		contentPaneCons.setWidth(Spring.sum(patientPaneCons.getConstraint(SpringLayout.WIDTH),
+												tabbedPaneCons.getConstraint(SpringLayout.WIDTH)));
+		patientPaneCons.setX(Spring.sum(Spring.constant(5), tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)));
+//		patientPaneCons.setY(Spring.sum(Spring.constant(5), tabbedPaneCons.getConstraint(SpringLayout.WIDTH)));
+		
+//		setContainerSize(contentPane, 5);
+		// display the frame in the middle of the screen
 		pack();
-		
 		Point screenCenterPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
 		setLocation(new Point (screenCenterPoint.x - getSize().width / 2,
 								screenCenterPoint.y - getSize().height / 2));
 		setVisible(true);
 	}
+	
+	protected class JExpander extends JPanel {
+		private static final long serialVersionUID = -6318804811000861743L;
+		private JButton m_toggle;
+		private JPanel m_control;
+		private JPanel m_content;
+		private boolean expanded;
+		
+		public JExpander() {
+			// set BorderLayout as main layout and activate double buffering
+			super(new BorderLayout(5, 5), true);
+			expanded = false;
+			
+			// create control panel with BorderLayout and add it
+			m_control = new JPanel(new BorderLayout(), true);
+			m_toggle = new JButton("Toggle");
+			m_toggle.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					toggle();
+				}
+			});
+			m_control.add(m_toggle, BorderLayout.WEST);
+			
+			// create content panel with GridLAyout and add it
+			GridLayout layout = new GridLayout();
+			layout.setHgap(5);
+			layout.setVgap(5);
+			m_content = new JPanel(new GridLayout(), true);
+		}
+		
+		public void toggle () {
+			m_content.setVisible(!expanded);
+			expanded = !expanded;
+			pack();
+		}
+		
+		public void expand() {
+			if (!expanded) {
+				toggle();
+			}
+		}
+		
+		public void reduce() {
+			if (expanded) {
+				toggle();
+			}
+		}
+		
+		
+	}
+
 	
 	protected JComponent makeTextPanel(String text) {
 		JPanel panel = new JPanel(false);
