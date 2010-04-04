07d80d2619138fb2088d9a22a52d1fa1d2308ad4
linustorvalds <heidixp@gmx.de>
2010-04-04 06:19:09 -0700

    JExpander is now working for standard frame size

--- src/dicomux/View.java
+++ src/dicomux/View.java
@@ -2,11 +2,11 @@ package dicomux;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Vector;
 
 import javax.swing.*;
 
 public class View extends JFrame {
-
 	private static final long serialVersionUID = -3586989981842552511L;
 	private JTabbedPane m_tabbedPane;
 	private JMenuBar m_menuBar;
@@ -42,25 +42,33 @@ public class View extends JFrame {
 		m_tabbedPane.addTab("Tab 1", panel1);
 		m_tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 		m_tabbedPane.setTabPlacement(JTabbedPane.LEFT);
+//		m_tabbedPane.setVisible(false);
 		contentPane.add(m_tabbedPane);
 		
 		// create a panel for the patient data and add it to the content pane
-		m_patientPanel = new JExpander();
-//		m_patientPanel.setPreferredSize(new Dimension(50, 50));
+		m_patientPanel = new JExpander(false, false);
 		contentPane.add(m_patientPanel);
 		
-		// adjust layout constraints of the components
+		// extract layout constraints from the components
 		SpringLayout.Constraints contentPaneCons = layout.getConstraints(contentPane);
 		SpringLayout.Constraints tabbedPaneCons = layout.getConstraints(m_tabbedPane);
 		SpringLayout.Constraints patientPaneCons = layout.getConstraints(m_patientPanel);
-		contentPaneCons.setHeight(Spring.sum(patientPaneCons.getConstraint(SpringLayout.HEIGHT),
-												tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)));
-		contentPaneCons.setWidth(Spring.sum(patientPaneCons.getConstraint(SpringLayout.WIDTH),
-												tabbedPaneCons.getConstraint(SpringLayout.WIDTH)));
-		patientPaneCons.setX(Spring.sum(Spring.constant(5), tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)));
-//		patientPaneCons.setY(Spring.sum(Spring.constant(5), tabbedPaneCons.getConstraint(SpringLayout.WIDTH)));
 		
-//		setContainerSize(contentPane, 5);
+		// adjust layout constraints of the contentPane
+		contentPaneCons.setHeight(Spring.sum(Spring.sum(patientPaneCons.getConstraint(SpringLayout.HEIGHT),
+														tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)),
+											Spring.constant(5)));
+		contentPaneCons.setWidth(Spring.sum(Spring.max(patientPaneCons.getConstraint(SpringLayout.WIDTH),
+														tabbedPaneCons.getConstraint(SpringLayout.WIDTH)),
+											Spring.constant(10)));
+		
+		// adjust layout constraints of the tabbedPane
+		tabbedPaneCons.setX(Spring.sum(contentPaneCons.getConstraint(SpringLayout.WEST),Spring.constant(5)));
+		
+		// adjust layout constraints of the patientPane
+		patientPaneCons.setX(Spring.sum(contentPaneCons.getConstraint(SpringLayout.WEST), Spring.constant(5)));
+		patientPaneCons.setY(Spring.sum(tabbedPaneCons.getConstraint(SpringLayout.SOUTH), Spring.constant(5)));
+		
 		// display the frame in the middle of the screen
 		pack();
 		Point screenCenterPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
@@ -69,58 +77,83 @@ public class View extends JFrame {
 		setVisible(true);
 	}
 	
+	/**
+	 * A JPanel derivative with a button for setting the content to visible or invisible
+	 * @author heidi
+	 *
+	 */
 	protected class JExpander extends JPanel {
 		private static final long serialVersionUID = -6318804811000861743L;
 		private JButton m_toggle;
 		private JPanel m_control;
 		private JPanel m_content;
-		private boolean expanded;
+		private boolean m_expanded;
 		
-		public JExpander() {
-			// set BorderLayout as main layout and activate double buffering
-			super(new BorderLayout(5, 5), true);
-			expanded = false;
-			
+		/**
+		 * initializes all components of the JExpander
+		 */
+		protected void initComponents(boolean isDoubleBuffered) {
 			// create control panel with BorderLayout and add it
-			m_control = new JPanel(new BorderLayout(), true);
-			m_toggle = new JButton("Toggle");
+			m_control = new JPanel(new BorderLayout(), isDoubleBuffered);
+			m_toggle = new JButton();
 			m_toggle.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					toggle();
 				}
 			});
-			m_control.add(m_toggle, BorderLayout.WEST);
+			m_control.add(m_toggle, BorderLayout.WEST); // add control button to a certain position
+			add(m_control, BorderLayout.NORTH);
+			add(new JLabel(),BorderLayout.CENTER);
 			
-			// create content panel with GridLAyout and add it
+			// create content panel with SpringLayout
 			GridLayout layout = new GridLayout();
-			layout.setHgap(5);
-			layout.setVgap(5);
-			m_content = new JPanel(new GridLayout(), true);
+			m_content = new JPanel(layout, isDoubleBuffered);
+
+			// Dummy content
+			m_content.add(new JLabel("Name: _______"));
+			m_content.add(new JLabel("Geburtsdatum: _______"));
+			add(m_content, BorderLayout.SOUTH);
 		}
 		
-		public void toggle () {
-			m_content.setVisible(!expanded);
-			expanded = !expanded;
-			pack();
+		/**
+		 * creates a new JExpander object
+		 * @param expanded determins wheather the new expander should be in expanded or shrinked mode
+		 */
+		public JExpander(boolean expanded, boolean isDoubleBuffered) {
+			// set BorderLayout as main layout and activate double buffering
+			super(new BorderLayout(), isDoubleBuffered);
+			
+			// initialize all components
+			initComponents(isDoubleBuffered);
+			
+			// switch to initial expander mode
+			setState(expanded);
 		}
 		
-		public void expand() {
-			if (!expanded) {
-				toggle();
-			}
+		/**
+		 * inverts the state of the expander
+		 */
+		public void toggle() {
+			setState(!m_expanded);
 		}
 		
-		public void reduce() {
-			if (expanded) {
-				toggle();
-			}
+		/**
+		 * sets the state of the expander directly
+		 * @param expanded
+		 */
+		public void setState(boolean expanded) {
+			m_expanded = expanded;
+			m_content.setVisible(m_expanded);
+			m_toggle.setText(m_expanded ? "Patientendaten verbergen" : "Patientendaten anzeigen");
 		}
-		
-		
 	}
 
-	
+	/**
+	 * convenience method for testing purpose
+	 * @param text String which will be shown in the center of the panel
+	 * @return a JPanel
+	 */
 	protected JComponent makeTextPanel(String text) {
 		JPanel panel = new JPanel(false);
 		JLabel filler = new JLabel(text);
