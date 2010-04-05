f60b7135967e6a0e362a49031d55454e270de5d9
linustorvalds <heidixp@gmx.de>
2010-04-05 16:41:32 +0200

    JExpander is now working for all frame sizes Welcome message added TODOs added

--- src/dicomux/View.java
+++ src/dicomux/View.java
@@ -2,27 +2,45 @@ package dicomux;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Vector;
 
 import javax.swing.*;
 
 public class View extends JFrame {
 	private static final long serialVersionUID = -3586989981842552511L;
+	
+	/**
+	 * contains the workspace tabbed pane
+	 */
 	private JTabbedPane m_tabbedPane;
+	
+	/**
+	 * contains the menu bar of the application
+	 */
 	private JMenuBar m_menuBar;
-	private JPanel m_patientPanel;
-
+	
+	/**
+	 * contains the expander for the patient specific data
+	 */
+	private JExpander m_patientPanel;
+	
+	//TODO outsource component initialization
+	/**
+	 * creates a new view
+	 */
 	public View() {
-		setTitle("dicomux");
+		setTitle("Dicomux");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// extract own contentPane and set its layout manager
 		Container contentPane = getContentPane();
-		SpringLayout layout = new SpringLayout();
-		contentPane.setLayout(layout);
+		contentPane.setLayout(new BorderLayout(0, 5));
 		
-		// create a main menu and fill in some entries
+		// create a main menu and add it to the View
 		m_menuBar = new JMenuBar();
+		setJMenuBar(m_menuBar);
+		
+		// ####### add dummy entries ######
+		//TODO remove
 		JMenu menu = new JMenu("File");
 		menu.add(new JMenuItem("Open"));
 		menu.addSeparator();
@@ -32,43 +50,24 @@ public class View extends JFrame {
 		menu = new JMenu("Help");
 		menu.add(new JMenuItem("About"));
 		m_menuBar.add(menu);
-		setJMenuBar(m_menuBar);
+		// ################################
 		
 		// create a tabbed pane and add it to the content pane
 		m_tabbedPane = new JTabbedPane();
-		m_tabbedPane.setPreferredSize(new Dimension(300, 200));
-		JComponent panel1 = makeTextPanel("Panel #1");
-//		panel1.setPreferredSize(new Dimension(200, 200));
-		m_tabbedPane.addTab("Tab 1", panel1);
-		m_tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+		contentPane.add(m_tabbedPane, BorderLayout.CENTER);
+		
+		// ########### add a dummy tab #####################
+		//TODO remove
+		JComponent panel1 = makeWelcomePanel();
+		m_tabbedPane.addTab("Welcome", panel1);
+		m_tabbedPane.setMnemonicAt(0, KeyEvent.VK_W);
 		m_tabbedPane.setTabPlacement(JTabbedPane.LEFT);
-//		m_tabbedPane.setVisible(false);
-		contentPane.add(m_tabbedPane);
+		// #################################################
 		
 		// create a panel for the patient data and add it to the content pane
 		m_patientPanel = new JExpander(false, false);
-		contentPane.add(m_patientPanel);
-		
-		// extract layout constraints from the components
-		SpringLayout.Constraints contentPaneCons = layout.getConstraints(contentPane);
-		SpringLayout.Constraints tabbedPaneCons = layout.getConstraints(m_tabbedPane);
-		SpringLayout.Constraints patientPaneCons = layout.getConstraints(m_patientPanel);
-		
-		// adjust layout constraints of the contentPane
-		contentPaneCons.setHeight(Spring.sum(Spring.sum(patientPaneCons.getConstraint(SpringLayout.HEIGHT),
-														tabbedPaneCons.getConstraint(SpringLayout.HEIGHT)),
-											Spring.constant(5)));
-		contentPaneCons.setWidth(Spring.sum(Spring.max(patientPaneCons.getConstraint(SpringLayout.WIDTH),
-														tabbedPaneCons.getConstraint(SpringLayout.WIDTH)),
-											Spring.constant(10)));
-		
-		// adjust layout constraints of the tabbedPane
-		tabbedPaneCons.setX(Spring.sum(contentPaneCons.getConstraint(SpringLayout.WEST),Spring.constant(5)));
-		
-		// adjust layout constraints of the patientPane
-		patientPaneCons.setX(Spring.sum(contentPaneCons.getConstraint(SpringLayout.WEST), Spring.constant(5)));
-		patientPaneCons.setY(Spring.sum(tabbedPaneCons.getConstraint(SpringLayout.SOUTH), Spring.constant(5)));
-		
+		contentPane.add(m_patientPanel, BorderLayout.SOUTH);
+
 		// display the frame in the middle of the screen
 		pack();
 		Point screenCenterPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
@@ -84,16 +83,33 @@ public class View extends JFrame {
 	 */
 	protected class JExpander extends JPanel {
 		private static final long serialVersionUID = -6318804811000861743L;
+		
+		/**
+		 * button for changing the mode of JExpander
+		 */
 		private JButton m_toggle;
+		
+		/**
+		 * contains the toggle 
+		 */
 		private JPanel m_control;
+		
+		/**
+		 * contains all content data
+		 */
 		private JPanel m_content;
+		
+		/**
+		 * current state of the expander
+		 */
 		private boolean m_expanded;
 		
+		//TODO outsource the EventHandler
 		/**
 		 * initializes all components of the JExpander
 		 */
 		protected void initComponents(boolean isDoubleBuffered) {
-			// create control panel with BorderLayout and add it
+			// create control panel with BorderLayout and add it to JExpander
 			m_control = new JPanel(new BorderLayout(), isDoubleBuffered);
 			m_toggle = new JButton();
 			m_toggle.addActionListener(new ActionListener() {
@@ -104,16 +120,16 @@ public class View extends JFrame {
 			});
 			m_control.add(m_toggle, BorderLayout.WEST); // add control button to a certain position
 			add(m_control, BorderLayout.NORTH);
-			add(new JLabel(),BorderLayout.CENTER);
 			
-			// create content panel with SpringLayout
+			// create content panel with GridLayout and add it to JExpander
 			GridLayout layout = new GridLayout();
 			m_content = new JPanel(layout, isDoubleBuffered);
-
-			// Dummy content
+			add(m_content, BorderLayout.SOUTH);
+			
+			// ############ add dummy content ####################
 			m_content.add(new JLabel("Name: _______"));
 			m_content.add(new JLabel("Geburtsdatum: _______"));
-			add(m_content, BorderLayout.SOUTH);
+			// ###################################################
 		}
 		
 		/**
@@ -121,13 +137,13 @@ public class View extends JFrame {
 		 * @param expanded determins wheather the new expander should be in expanded or shrinked mode
 		 */
 		public JExpander(boolean expanded, boolean isDoubleBuffered) {
-			// set BorderLayout as main layout and activate double buffering
+			// set BorderLayout as main layout and set double buffering
 			super(new BorderLayout(), isDoubleBuffered);
 			
 			// initialize all components
 			initComponents(isDoubleBuffered);
 			
-			// switch to initial expander mode
+			// switch to initial JExpander mode
 			setState(expanded);
 		}
 		
@@ -138,6 +154,7 @@ public class View extends JFrame {
 			setState(!m_expanded);
 		}
 		
+		//TODO localization needed
 		/**
 		 * sets the state of the expander directly
 		 * @param expanded
@@ -145,21 +162,47 @@ public class View extends JFrame {
 		public void setState(boolean expanded) {
 			m_expanded = expanded;
 			m_content.setVisible(m_expanded);
-			m_toggle.setText(m_expanded ? "Patientendaten verbergen" : "Patientendaten anzeigen");
+			m_toggle.setText(m_expanded ? "Hide patient data" : "Show patient data");
 		}
 	}
-
+	
+	//TODO localization needed / cleanup
 	/**
 	 * convenience method for testing purpose
-	 * @param text String which will be shown in the center of the panel
 	 * @return a JPanel
 	 */
-	protected JComponent makeTextPanel(String text) {
-		JPanel panel = new JPanel(false);
+	protected JComponent makeWelcomePanel() {
+		JPanel content = new JPanel(new BorderLayout(5 , 5), false);
+		JPanel contentHead = new JPanel(new BorderLayout(5, 0), false);
+		content.add(contentHead, BorderLayout.NORTH);
+		
+		JPanel message = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0),false);
+		String text = "<html><font size=\"+2\">Welcome to Dicomux</font><br/><i>The free ecg viewer for DICOM files.</i><br/><br/>You may want to do one of the following things:</html>";
 		JLabel filler = new JLabel(text);
-		filler.setHorizontalAlignment(JLabel.CENTER);
-		panel.setLayout(new GridLayout(1, 1));
-		panel.add(filler);
-		return panel;
+		message.add(filler, BorderLayout.WEST);
+		contentHead.add(message, BorderLayout.NORTH);
+		
+		JPanel control = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0), false);
+		control.add(new JButton("Open DICOM file")/*, BorderLayout.WEST*/);
+		control.add(new JButton("Open DICOM directory")/*, BorderLayout.EAST*/);
+		contentHead.add(control, BorderLayout.SOUTH);
+		
+		return content;
+	}
+	
+	/**
+	 * convenience method for opening ImageIcons // copied from http://java.sun.com/docs/books/tutorial/uiswing/components/icon.html
+	 * @param path path to the icon file
+	 * @param description description of the file
+	 * @return the ImageIcon of the opened file or null
+	 */
+	protected ImageIcon createImageIcon(String path, String description) {
+		java.net.URL imgURL = getClass().getResource(path);
+		if (imgURL != null)
+			return new ImageIcon(imgURL, description);
+		else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
 	}
 }
