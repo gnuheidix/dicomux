ddba38f8ede1d65c4419a83c530cff9d497f3047
linustorvalds <heidixp@gmx.de>
2010-04-06 12:20:28 +0200

    JExpander with patient specific data removed Plug-in concept introduced

--- src/dicomux/View.java
+++ src/dicomux/View.java
@@ -18,10 +18,10 @@ public class View extends JFrame {
 	 */
 	private JMenuBar m_menuBar;
 	
-	/**
-	 * contains the expander for the patient specific data
-	 */
-	private JExpander m_patientPanel;
+//	/**
+//	 * contains the expander for the patient specific data
+//	 */
+//	private JExpander m_patientPanel;
 	
 	//TODO outsource component initialization
 	/**
@@ -54,6 +54,7 @@ public class View extends JFrame {
 		
 		// create a tabbed pane and add it to the content pane
 		m_tabbedPane = new JTabbedPane();
+		m_tabbedPane.setTabPlacement(JTabbedPane.TOP);
 		contentPane.add(m_tabbedPane, BorderLayout.CENTER);
 		
 		// ########### add a dummy tab #####################
@@ -61,12 +62,11 @@ public class View extends JFrame {
 		JComponent panel1 = makeWelcomePanel();
 		m_tabbedPane.addTab("Welcome", panel1);
 		m_tabbedPane.setMnemonicAt(0, KeyEvent.VK_W);
-		m_tabbedPane.setTabPlacement(JTabbedPane.LEFT);
 		// #################################################
 		
-		// create a panel for the patient data and add it to the content pane
-		m_patientPanel = new JExpander(false, false);
-		contentPane.add(m_patientPanel, BorderLayout.SOUTH);
+//		// create a panel for the patient data and add it to the content pane
+//		m_patientPanel = new JExpander(false, false);
+//		contentPane.add(m_patientPanel, BorderLayout.SOUTH);
 
 		// display the frame in the middle of the screen
 		pack();
@@ -76,95 +76,95 @@ public class View extends JFrame {
 		setVisible(true);
 	}
 	
-	/**
-	 * A JPanel derivative with a button for setting the content to visible or invisible
-	 * @author heidi
-	 *
-	 */
-	protected class JExpander extends JPanel {
-		private static final long serialVersionUID = -6318804811000861743L;
-		
-		/**
-		 * button for changing the mode of JExpander
-		 */
-		private JButton m_toggle;
-		
-		/**
-		 * contains the toggle 
-		 */
-		private JPanel m_control;
-		
-		/**
-		 * contains all content data
-		 */
-		private JPanel m_content;
-		
-		/**
-		 * current state of the expander
-		 */
-		private boolean m_expanded;
-		
-		//TODO outsource the EventHandler
-		/**
-		 * initializes all components of the JExpander
-		 */
-		protected void initComponents(boolean isDoubleBuffered) {
-			// create control panel with BorderLayout and add it to JExpander
-			m_control = new JPanel(new BorderLayout(), isDoubleBuffered);
-			m_toggle = new JButton();
-			m_toggle.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					toggle();
-				}
-			});
-			m_control.add(m_toggle, BorderLayout.WEST); // add control button to a certain position
-			add(m_control, BorderLayout.NORTH);
-			
-			// create content panel with GridLayout and add it to JExpander
-			GridLayout layout = new GridLayout();
-			m_content = new JPanel(layout, isDoubleBuffered);
-			add(m_content, BorderLayout.SOUTH);
-			
-			// ############ add dummy content ####################
-			m_content.add(new JLabel("Name: _______"));
-			m_content.add(new JLabel("Geburtsdatum: _______"));
-			// ###################################################
-		}
-		
-		/**
-		 * creates a new JExpander object
-		 * @param expanded determins wheather the new expander should be in expanded or shrinked mode
-		 */
-		public JExpander(boolean expanded, boolean isDoubleBuffered) {
-			// set BorderLayout as main layout and set double buffering
-			super(new BorderLayout(), isDoubleBuffered);
-			
-			// initialize all components
-			initComponents(isDoubleBuffered);
-			
-			// switch to initial JExpander mode
-			setState(expanded);
-		}
-		
-		/**
-		 * inverts the state of the expander
-		 */
-		public void toggle() {
-			setState(!m_expanded);
-		}
-		
-		//TODO localization needed
-		/**
-		 * sets the state of the expander directly
-		 * @param expanded
-		 */
-		public void setState(boolean expanded) {
-			m_expanded = expanded;
-			m_content.setVisible(m_expanded);
-			m_toggle.setText(m_expanded ? "Hide patient data" : "Show patient data");
-		}
-	}
+//	/**
+//	 * A JPanel derivative with a button for setting the content to visible or invisible
+//	 * @author heidi
+//	 *
+//	 */
+//	protected class JExpander extends JPanel {
+//		private static final long serialVersionUID = -6318804811000861743L;
+//		
+//		/**
+//		 * button for changing the mode of JExpander
+//		 */
+//		private JButton m_toggle;
+//		
+//		/**
+//		 * contains the toggle 
+//		 */
+//		private JPanel m_control;
+//		
+//		/**
+//		 * contains all content data
+//		 */
+//		private JPanel m_content;
+//		
+//		/**
+//		 * current state of the expander
+//		 */
+//		private boolean m_expanded;
+//		
+//		//TODO outsource the EventHandler
+//		/**
+//		 * initializes all components of the JExpander
+//		 */
+//		protected void initComponents(boolean isDoubleBuffered) {
+//			// create control panel with BorderLayout and add it to JExpander
+//			m_control = new JPanel(new BorderLayout(), isDoubleBuffered);
+//			m_toggle = new JButton();
+//			m_toggle.addActionListener(new ActionListener() {
+//				@Override
+//				public void actionPerformed(ActionEvent e) {
+//					toggle();
+//				}
+//			});
+//			m_control.add(m_toggle, BorderLayout.WEST); // add control button to a certain position
+//			add(m_control, BorderLayout.NORTH);
+//			
+//			// create content panel with GridLayout and add it to JExpander
+//			GridLayout layout = new GridLayout();
+//			m_content = new JPanel(layout, isDoubleBuffered);
+//			add(m_content, BorderLayout.SOUTH);
+//			
+//			// ############ add dummy content ####################
+//			m_content.add(new JLabel("Name: _______"));
+//			m_content.add(new JLabel("Geburtsdatum: _______"));
+//			// ###################################################
+//		}
+//		
+//		/**
+//		 * creates a new JExpander object
+//		 * @param expanded determins wheather the new expander should be in expanded or shrinked mode
+//		 */
+//		public JExpander(boolean expanded, boolean isDoubleBuffered) {
+//			// set BorderLayout as main layout and set double buffering
+//			super(new BorderLayout(), isDoubleBuffered);
+//			
+//			// initialize all components
+//			initComponents(isDoubleBuffered);
+//			
+//			// switch to initial JExpander mode
+//			setState(expanded);
+//		}
+//		
+//		/**
+//		 * inverts the state of the expander
+//		 */
+//		public void toggle() {
+//			setState(!m_expanded);
+//		}
+//		
+//		//TODO localization needed
+//		/**
+//		 * sets the state of the expander directly
+//		 * @param expanded
+//		 */
+//		public void setState(boolean expanded) {
+//			m_expanded = expanded;
+//			m_content.setVisible(m_expanded);
+//			m_toggle.setText(m_expanded ? "Hide patient data" : "Show patient data");
+//		}
+//	}
 	
 	//TODO localization needed / cleanup
 	/**
@@ -177,7 +177,7 @@ public class View extends JFrame {
 		content.add(contentHead, BorderLayout.NORTH);
 		
 		JPanel message = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0),false);
-		String text = "<html><font size=\"+2\">Welcome to Dicomux</font><br/><i>The free ecg viewer for DICOM files.</i><br/><br/>You may want to do one of the following things:</html>";
+		String text = "<html><font size=\"+2\">Welcome to Dicomux</font><br/><i>The free ECG viewer for DICOM files.</i><br/><br/>You may want to do one of the following things:</html>";
 		JLabel filler = new JLabel(text);
 		message.add(filler, BorderLayout.WEST);
 		contentHead.add(message, BorderLayout.NORTH);
